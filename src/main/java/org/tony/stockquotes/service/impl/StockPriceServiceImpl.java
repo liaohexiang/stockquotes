1750fda8742b633e1fb21cb299e67b65b5763c9f
liaohexiang <liaohexiang@gmail.com>
2023-07-27 09:38:32 +0800

    update

--- src/main/java/org/tony/stockquotes/service/impl/StockPriceServiceImpl.java
+++ src/main/java/org/tony/stockquotes/service/impl/StockPriceServiceImpl.java
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class StockPriceServiceImpl extends ServiceImpl<StockPriceMapper, StockPrice>
     implements StockPriceService{
-
+     public void main(String[] args){
+         System.out.println("yes");
+     }
 }
 
 
